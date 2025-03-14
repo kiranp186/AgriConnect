f87a92a4b100e605a31abe998ed57832948e497c
kiranp186 <kpkiranp186@gmail.com>
2025-03-14 23:41:43 +0530

    equipment page

--- app/src/main/java/com/tutorials/agriconnect/EquipmentPage.kt
+++ app/src/main/java/com/tutorials/agriconnect/EquipmentPage.kt
@@ -1,6 +1,5 @@
 package com.tutorials.agriconnect
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -21,8 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
